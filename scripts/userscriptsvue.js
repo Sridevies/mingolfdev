b2a334a65ab01cec851d2c27d4f37473df247a08
Sridevies <30491104+Sridevies@users.noreply.github.com>
2020-08-10 16:48:29 +0530

    Update userscriptsvue.js

--- scripts/userscriptsvue.js
+++ scripts/userscriptsvue.js
@@ -126,6 +126,7 @@ function refreshSlider(mode){
 				  dots: true,
 				  infinite: false,
 				  speed: 300,
+				  autoplay:cls.indexOf("first") !=-1 ? true:false,
 				  slidesToShow: 3,
 				  slidesToScroll: 3,
 				  responsive: [{breakpoint: 1024,settings: {slidesToShow: 3,slidesToScroll: 3,infinite: true,dots: true}},{breakpoint: 980,settings: {slidesToShow: 2,slidesToScroll: 2}},{breakpoint:600,settings: {slidesToShow: 1,slidesToScroll: 1}}]
